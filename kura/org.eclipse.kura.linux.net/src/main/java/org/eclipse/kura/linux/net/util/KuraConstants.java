722d2bb613fe143e53ac170d0b5dee7ef6ab544d
Chris Custine <ccustine@apache.org>
2016-05-24 09:45:57 -0600

    Add RHEL_Generic profile configuration

--- kura/org.eclipse.kura.linux.net/src/main/java/org/eclipse/kura/linux/net/util/KuraConstants.java
+++ kura/org.eclipse.kura.linux.net/src/main/java/org/eclipse/kura/linux/net/util/KuraConstants.java
@@ -23,7 +23,8 @@ public enum KuraConstants {
 	BeagleBone ("debian", "1.0.0", "beaglebone"),
 	ReliaGATE_50_21_Ubuntu("ubuntu", "14.04", "reliagate-50-21"),
 	Reliagate_10_11("yocto", "1.2.1", "reliagate-10-11"),
-    Reliagate_20_25("yocto", "1.2.1", "reliagate-20-25");
+	Reliagate_20_25("yocto", "1.2.1", "reliagate-20-25"),
+	RHEL_Generic("rhel", "7.2", "generic");
 	
 	private String m_imageName;
 	private String m_imageVersion;
